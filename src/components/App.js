d9c7e53c5b09a2a8004b66cfa4912cb1c174319c
MD IMTIYAZ <64704175+Imtiyaz-78@users.noreply.github.com>
2022-12-31 15:19:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,13 @@
-import React from "react";
+import React, {Component, useState} from "react";
+import '../styles/App.css';
 
-// const App=()=>{
-//   return(<>
-//          <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-//         </>)
-//       }
-         
-// export default App; 
-         
 const App = () => {
+  return (
+    <div id="main">
+    <p>I am learning React. My life is getting better</p>
+    </div>
+  )
+}
 
- return ( 
 
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> 
-
-)
-
- }
-
-export default App; 
-
+export default App;
